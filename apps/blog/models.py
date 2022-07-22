fbbaa55bbf04375bb1700824e5f91f0a748442d4
Artem <artem.terentyev06@gmail.com>
2022-07-22 13:42:45 +0300

    tags migrations2

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(verbose_name='Название тега', max_length=255)
+    name = models.CharField(verbose_name='Название тега', max_length=255, blank=True)
 
     def __str__(self):
         return self.name
