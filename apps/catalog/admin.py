e736c75001e495f8b4d5c72e00b78fe1a366e2f5
Artem <artem.terentyev06@gmail.com>
2022-08-14 13:41:02 +0300

    dz16

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from apps.catalog.models import Category
+from apps.catalog.models import Category, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'quantity', 'price', 'created_at', 'updated_at']
+    fields = ['name', 'description', 'quantity', 'price']
