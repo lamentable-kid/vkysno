62bec89e195fdd62fcfae175e7867691d3e7c53f
Artem <artem.terentyev06@gmail.com>
2022-08-17 18:35:20 +0300

    product_to_category

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -7,7 +7,13 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
 
 
+class ProductCategoryInline(admin.TabularInline):
+    model = Product.categories.through
+    extra = 1
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'quantity', 'price', 'created_at', 'updated_at']
     fields = ['name', 'description', 'quantity', 'price']
+    inlines = [ProductCategoryInline]
