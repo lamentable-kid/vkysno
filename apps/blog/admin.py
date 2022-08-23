2b69ccdf19fc0c572a24cd1bd2df6b7a7abee64e
Artem <artem.terentyev06@gmail.com>
2022-08-23 21:10:26 +0300

    dz19

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -9,7 +9,7 @@ from django.utils.http import urlencode
 class BlogCategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'image_tag_thumbnail', 'article_list_link']
     list_display_links = ['id', 'name', 'image_tag_thumbnail']
-    fields = ['name', 'image_tag', 'image']
+    fields = ['name', 'image_tag', 'image', 'meta_title', 'meta_description', 'meta_keywords']
     readonly_fields = ['image_tag']
 
     def article_list_link(self, obj):
@@ -28,7 +28,8 @@ class BlogCategoryAdmin(admin.ModelAdmin):
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'image_tag_thumbnail', 'category_link', 'created_at', 'tags_list']
     list_display_links = ['id', 'title', 'image_tag_thumbnail', 'tags_list']
-    fields = ['category', 'image_tag', 'image', 'tags', 'title', 'text_preview', 'text', 'user']
+    fields = ['category', 'image_tag', 'image', 'tags', 'title', 'text_preview', 'text', 'user', 'meta_title',
+              'meta_description', 'meta_keywords']
     readonly_fields = ['image_tag']
     list_filter = ['category', 'tags']
 
@@ -52,7 +53,9 @@ class ArticleAdmin(admin.ModelAdmin):
 
     tags_list.short_description = 'Теги'
 
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ['name']
     list_display_links = ['name']
+    fields = ['meta_title', 'meta_description', 'meta_keywords']
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.utils.safestring import mark_safe
 from imagekit.models import ProcessedImageField, ImageSpecField
 from pilkit.processors import ResizeToFill
+
+from apps.main.mixins import MetaTagMixin
 from apps.user.models import User
 from config.settings import MEDIA_ROOT
 
 
-class BlogCategory(models.Model):
+class BlogCategory(MetaTagMixin):
     name = models.CharField(verbose_name='Название', max_length=255)
     # image = models.ImageField(verbose_name='Изображение', upload_to='blog/category/', null=True)
     image = ProcessedImageField(
@@ -40,7 +42,7 @@ class BlogCategory(models.Model):
         verbose_name_plural = 'Категории блога'
 
 
-class Tag(models.Model):
+class Tag(MetaTagMixin):
     name = models.CharField(verbose_name='Название', max_length=255)
 
     def __str__(self):
@@ -51,7 +53,7 @@ class Tag(models.Model):
         verbose_name_plural = 'Тэги'
 
 
-class Article(models.Model):
+class Article(MetaTagMixin):
     category = models.ForeignKey(
         to=BlogCategory,
         verbose_name='Категория',
@@ -104,6 +106,11 @@ class Article(models.Model):
     image_tag.allow_tags = True
 
     def __str__(self):
+        return self.title
+
+    def get_meta_title(self):
+        if self.meta_title:
+            return self.meta_title
         return self.title
 
     class Meta:
